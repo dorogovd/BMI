d66d82efbe05768542549549ede20e7618586dc4
Dima <dorogovd@yandex.ru>
2023-11-12 22:20:39 +0300

    final update

--- BMI Calculator/Controllers/CalculatorBrain.swift
+++ BMI Calculator/Controllers/CalculatorBrain.swift
@@ -6,18 +6,34 @@
 //  Copyright © 2023 Angela Yu. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 struct CalculatorBrain {
     
-    var bmi: Float?
-    
-    mutating func calculateBMI(height: Float, weight: Float) {
-        bmi = weight / (height * height)
-    }
+    var bmi: BMI?
     
     mutating func getBMIValue() -> String {
-        let bmiTo1DecimalPlace = String(format: "%.1f", bmi ?? 0.0)
+        let bmiTo1DecimalPlace = String(format: "%.1f", bmi?.value ?? 0.0)
         return bmiTo1DecimalPlace
+    }
+    
+    func getAdvice() -> String {
+        return bmi?.advice ?? "00"
+    }
+    
+    func getColor() -> UIColor {
+        return bmi?.color ?? .black
+    }
+    
+    mutating func calculateBMI(height: Float, weight: Float) {
+        let bmiValue = weight / (height * height)
+        
+        if bmiValue < 18.5 {
+            bmi = BMI(value: bmiValue, advice: "Eat more pies!", color: UIColor.blue)
+        } else if bmiValue < 24.9 {
+            bmi = BMI(value: bmiValue, advice: "Fit as a fiddle!", color: UIColor.green)
+        } else {
+            bmi = BMI(value: bmiValue, advice: "u r a phat piatch!", color: UIColor.orange)
+        }
     }
 }
